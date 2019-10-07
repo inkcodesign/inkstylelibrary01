6e31313a5ff73eeac2c161a154fe92dd5d305527
stephenbarkan <stephenbarkan@gmail.com>
2019-10-07 17:18:43 -0500

    added barba.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,7 @@
 const sidebar = document.querySelector('.sidebar')
 const main = document.querySelector('.main')
-
+const sidebarLinks = sidebar.querySelectorAll('.sidebar__link')
+const body = document.querySelector('body')
 
 const setOffset = function () {
     const sidebarWidth = sidebar.getBoundingClientRect().width + "px"
@@ -16,22 +17,31 @@ window.addEventListener('resize', function () {
 
 
 
-// // import barba from '@barba/core';
+// import barba from '@barba/core';
 
-// // basic default transition (with no rules and minimal hooks)
-// barba.init({
-//     transitions: [{
-//         leave({ current, next, trigger }) {
-//             // do something with `current.container` for your leave transition
-//             // then return a promise or use `this.async()`
-//         },
-//         afterEnter({ current, next, trigger }) {
-//             // do something with `next.container` for your enter transition
-//             // then return a promise or use `this.async()`
+// basic default transition (with no rules and minimal hooks)
+barba.init({
+    transitions: [{
+        leave({ current, next, trigger }) {
+            // do something with `current.container` for your leave transition
+            // then return a promise or use `this.async()`
+        },
+        afterEnter({ next }) {
+            // do something with `next.container` for your enter transition
+            // then return a promise or use `this.async()`
+            // console.log(data.current.url)
 
-//             const html = document.querySelector('html')
-//             delete html.dataset.open
-//         }
-//     }]
-// });
+
+            const html = document.querySelector('html')
+            delete html.dataset.open
+            body.scrollTop = 0
+            sidebarLinks.forEach(link => {
+                delete link.dataset.active
+                if (next.url.href === link.getAttribute('href')) {
+                    link.dataset.active = ""
+                }
+            });
+        }
+    }]
+});
 
